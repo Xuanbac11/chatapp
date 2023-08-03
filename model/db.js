6665ab56bcc83fa99ed4c061a8fc20d28933f983
bacnvmomo <bacnv@mo.com.mm>
2023-08-03 16:07:55 +0700

    fix bug

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://vanbac1108z01:NguyenVanBac123@cluster0.oxhsbbn.mongodb.net/asm").catch((err) => {
+mongoose.connect("mongodb+srv://vanbac1108z01:NguyenVanBac123@cluster0.oxhsbbn.mongodb.net/asm?retryWrites=true&w=majority").catch((err) => {
   console.log("Loi CSDL: ", err);
 });
 module.exports = { mongoose };
