7d3459cbee870fdb31ca8498f0204140743ebc0c
bacnvmomo <bacnv@mo.com.mm>
2023-08-03 15:38:56 +0700

    change link api

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/asm").catch((err) => {
+mongoose.connect("mongodb+srv://vanbac1108z01:NguyenVanBac123@cluster0.oxhsbbn.mongodb.net/asm").catch((err) => {
   console.log("Loi CSDL: ", err);
 });
 module.exports = { mongoose };
